308eb57edf5d57ac6ea1cb16c020477e9ead8264
Hamza Maach <hamzamaach56@gmail.com>
2024-02-13 15:54:56 +0100

    who are you 2

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,3 +1,3 @@
-name=$(curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq -r '.[] | select(.id == 70) | .name' )
+name=$(curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq  '.[] | select(.id == 70) | .name' )
 
 echo "$name"
