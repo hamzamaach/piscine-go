f37a515b4d8e3abcfd4b8c7c789d79fbde37520d
Hamza Maach <hamzamaach56@gmail.com>
2024-02-13 16:28:03 +0100

    to-git-or-not-to-git.sh 2

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,6 +1,4 @@
 name=$(curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq -r '.[] | select(.id ==170 ) | .name')
 power=$(curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq '.[] | select(.id ==170 ) | .powerstats.power')
 gender=$(curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq -r '.[] | select(.id ==170 ) | .appearance.gender')
-echo "$name"
-echo "$power"
-echo "$gender"
+printf "%s\n%s\n%s\n" "$name" "$power" "$gender"
