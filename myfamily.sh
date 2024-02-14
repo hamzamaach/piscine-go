a22a1354a52e1bbfe9537e576217c17d15ca9a41
Hamza Maach <hamzamaach56@gmail.com>
2024-02-14 17:36:51 +0100

    cl-camp4 7

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,1 @@
-allRelatives=$(curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq -r ".[] | select(.id==$HERO_ID) | .connections.relatives")
-echo $allRelatives
+curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq ".[] | select(.id==$HERO_ID) | .connections.relatives" | tr -d '"'
