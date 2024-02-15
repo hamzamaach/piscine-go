aeb4199889dbdf5f4fb186872ac3f5961f473c82
Hamza Maach <hamzamaach56@gmail.com>
2024-02-15 18:13:08 +0100

    print comb2 8

--- printcomb2.go
+++ printcomb2.go
@@ -5,22 +5,22 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for a := '0'; a <= '9'; a++ {
 		for b := '0'; b <= '8'; b++ {
+			d := '1'
 			for c := '0'; c <= '9'; c++ {
-				for d := '1'; d <= '9'; d++ {
+				for ; d <= '9'; d++ {
 					if a < c || b < d && a == c {
-						if a != '9' || b != '9' {
-							z01.PrintRune(a)
-							z01.PrintRune(b)
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(' ')
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						if a != '9' || b != '8' || c != '9' || d != '9' {
+							z01.PrintRune(',')
 							z01.PrintRune(' ')
-							z01.PrintRune(c)
-							z01.PrintRune(d)
-							if a != '9' || b != '8' || c != '9' || d != '9' {
-								z01.PrintRune(',')
-								z01.PrintRune(' ')
-							}
 						}
 					}
 				}
+				d = '0'
 			}
 		}
 	}
