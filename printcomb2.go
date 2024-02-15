e557ad6ce5cccbe4eca2b0620f9774fd5c6e84b6
Hamza Maach <hamzamaach56@gmail.com>
2024-02-15 17:21:38 +0100

    print comb2 6

--- printcomb2.go
+++ printcomb2.go
@@ -7,8 +7,7 @@ func PrintComb2() {
 		for b := '0'; b <= '8'; b++ {
 			for c := '0'; c <= '9'; c++ {
 				for d := '1'; d <= '9'; d++ {
-					if a <= c && b < d {
-
+					if a <= c || b < d {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
