7abe360a5126b07affb1a298c29e2752d22008ea
Hamza Maach <hamzamaach56@gmail.com>
2024-02-17 11:34:36 +0100

    convert a String to an Int using strconv.Atoi 2

--- basicatoi.go
+++ basicatoi.go
@@ -1,18 +1,11 @@
 package piscine
 
-// import "fmt"
-
-// import "strconv"
-
 func BasicAtoi(s string) int {
-	var strToInt int 
-	// i, _ := strconv.Atoi(s)
-	// stringToBytes := []byte(s)
-	// fmt.Println(int(s))
+	var strToInt int
 	for _, char := range s {
 		if char >= '0' && char <= '9' {
 			strToInt = (strToInt * 10) + int(char-'0')
-		}else {
+		} else {
 			return 0
 		}
 	}
