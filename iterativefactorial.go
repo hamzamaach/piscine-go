8655167d5dbcb40536e28ffd93284aed324bdbc1
Hamza Maach <hamzamaach56@gmail.com>
2024-02-20 13:19:58 +0100

    iterative factorial 4

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,16 +1,17 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	result := 1
-	if nb > 0 {
+	if nb < 0 {
+		return 0
+	} else {
+		result := 1
 		for i := 1; i <= nb; i++ {
 			result *= i
 		}
 		if result <= 0 {
 			return 0
+		} else {
+			return result
 		}
-		return result
-	} else {
-		return 0
 	}
 }
