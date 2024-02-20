51584cfd76cd7d35c43b60872fa92fc3d6793c33
Hamza Maach <hamzamaach56@gmail.com>
2024-02-20 14:02:05 +0100

    recursive factorial 2

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,18 +1,18 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
+	maxNumber := 20
 	if nb < 0 {
 		return 0
 	} else if nb == 0 {
 		return 1
 	} else {
-		result := 1
-		result = nb * RecursiveFactorial(nb-1)
-		if result > 0 {
+		if nb > 0 && nb <= maxNumber {
+			result := 1
+			result = nb * RecursiveFactorial(nb-1)
 			return result
 		} else {
 			return 0
 		}
-
 	}
 }
