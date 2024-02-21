e154def93ef8ea27d8b3c8451c58d0aa1019cdbe
Hamza Maach <hamzamaach56@gmail.com>
2024-02-21 18:12:52 +0100

    write a function check if the number given is prime

--- isprime.go
+++ isprime.go
@@ -1,18 +1,15 @@
 package piscine
 
-
 func IsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
 	}
-	/* get the square root of nb to use it the next loop */
 	var sqrt int
 	for i := 1; i <= nb; i++ {
 		if nb/i == i && nb%i == 0 {
 			sqrt = i
 		}
 	}
-	
 	for i := 2; i <= sqrt; i++ {
 		if nb%i == 0 {
 			return false
