bdc26b3ac01064510b556b0fce330f9fb2c8f95f
Hamza Maach <hamzamaach56@gmail.com>
2024-02-22 12:15:02 +0100

    function that compare two strings given

--- compare.go
+++ compare.go
@@ -1,6 +1,17 @@
 package piscine
 
 func Compare(a, b string) int {
+	var minLength int
+	if len(a) > len(b) {
+		minLength = len(b)
+	} else {
+		minLength = len(a)
+	}
+	for i := 0; i < minLength; i++ {
+		if a[i] < b[i] {
+			return -1
+		}
+	}
 	if len(a) > len(b) {
 		return 1
 	} else if len(a) < len(b) {
