81550663413dcb40b6dbfad919ff5110c3b987be
Hamza Maach <hamzamaach56@gmail.com>
2024-02-22 14:12:46 +0100

    check if all the chars are alphanumerical characters in string given

--- isalpha.go
+++ isalpha.go
@@ -3,7 +3,7 @@ package piscine
 func IsAlpha(s string) bool {
 	isAlpha := false
 	for _, char := range s {
-		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '1' && char <= '9') {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9') {
 			isAlpha = true
 		} else {
 			return false
