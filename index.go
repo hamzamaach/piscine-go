3a7ebd1a5992db530ff24c77bba43223db953fe3
Hamza Maach <hamzamaach56@gmail.com>
2024-02-22 13:39:32 +0100

    get index of an substring in string

--- index.go
+++ index.go
@@ -1,14 +1,13 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	
 	for i := 0; i < len(s); i++ {
 		found := true
 		for j := 0; j < len(toFind); j++ {
 			if s[i+j] != toFind[j] {
 				found = false
 				break
-			} 
+			}
 		}
 		if found {
 			return i
