dae09d8b2b33286fe6d1d18d45cf659401258ff0
Hamza MAACH <hamzamaach56@gmail.com>
2024-02-23 12:05:55 +0100

    Function that order an int given

---
+++ printnbrinorder.go
@@ -0,0 +1,29 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+	} else if n >= 0 {
+		var result []int
+		for n > 0 {
+			digit := n % 10
+			result = append(result, digit)
+			n = n / 10
+		}
+		for j := 0; j < len(result); j++ {
+			for i := 0; i < len(result)-1; i++ {
+				if result[i] > result[i+1] {
+					saveTemp := result[i]
+					result[i] = result[i+1]
+					result[i+1] = saveTemp
+				}
+			}
+		}
+		for i := len(result) - 1; i >= 0; i-- {
+			nb := '0' + result[i]
+			z01.PrintRune(rune(nb))
+		}
+	}
+}
