4104465331ffef88e52f153fd9b18b097bc6de48
hamzamaach <hamzamaach>
2024-02-25 16:54:02 +0100

    concat params given as a slice of strings

---
+++ concatparams.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	result := ""
+	for i, word := range args {
+		if i!=len(args)-1{
+			result += word + "\n"
+			}else{
+			result += word
+		}
+	}
+	return result
+}
