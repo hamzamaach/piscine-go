a48abf85c8cda536a12762e4a002e419a954f8b8
hamzamaach <hamzamaach>
2024-02-25 16:44:47 +0100

    append range of min and max using make()

---
+++ makerange.go
@@ -0,0 +1,14 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	size := max - min
+	if size > 0 {
+		result := make([]int, size)
+		for i := 0; i < size; i++ {
+			result[i] = min + i
+		}
+		return result
+	} else {
+		return []int{}
+	}
+}
