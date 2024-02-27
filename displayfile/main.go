a012f92c499b3592df412c78495b93671b3dccdf
Hamza Maach <hamzamaach56@gmail.com>
2024-02-27 15:49:08 +0100

    display content of a file

---
+++ displayfile/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Printf("File name missing")
+		fmt.Printf("\n")
+	} else if len(args) > 1 {
+		fmt.Printf("Too many arguments")
+		fmt.Printf("\n")
+	} else {
+		content,_ :=  ioutil.ReadFile(args[0])
+		fmt.Println(string(content))
+	}
+}
