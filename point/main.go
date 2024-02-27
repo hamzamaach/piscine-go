5d88610130625dbd7976d8d6c388ea3e91120adf
hamzamaach <hamzamaach>
2024-02-27 13:53:06 +0100

    print runes

--- point/main.go
+++ point/main.go
@@ -1,61 +1,11 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
-type point struct {
-	x int
-	y int
-}
-
-func setPoint(ptr *point) {
-	ptr.x = 42
-	ptr.y = 21
-}
-
-func printChar(char rune) {
-	z01.PrintRune(char)
-}
-
-func printString(str string) {
+func main() {
+	str := "x = 42, y = 21"
 	for _, char := range str {
 		z01.PrintRune(char)
 	}
-}
-
-func intToString(num int) string {
-	var result [19]byte
-	n := len(result) - 1
-	if num >= 0 {
-
-		if num == 0 {
-			return "0"
-		}
-		for num > 0 {
-			mod := num % 10
-			result[n] = byte(mod) + '0'
-			num /= 10
-			n--
-		}
-	}
-	return string(result[n+1:])
-}
-
-func printPoint(varName rune, varValue int) {
-	printChar(varName)
-	printChar(' ')
-	printChar('=')
-	printChar(' ')
-	printString(intToString(varValue))
-}
-
-func main() {
-	points := point{}
-	setPoint(&points)
-	printPoint('x', points.x)
-	printChar(',')
-	printChar(' ')
-	printPoint('y', points.y)
 	z01.PrintRune('\n')
 }
