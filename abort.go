723f18ef5eac1c69d22cde65d011ec65fd8c7e39
Hamza Maach <hamzamaach56@gmail.com>
2024-02-29 15:14:55 +0100

    func eturns the median of five int arguments given

--- abort.go
+++ abort.go
@@ -1,5 +1,15 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	return (a + b + c + d + e) / 5
+	numbers := []int{a, b, c, d, e}
+	for i := 0; i < len(numbers)-1; i++ {
+		for j := i + 1; j < len(numbers); j++ {
+			if numbers[i] > numbers[j] {
+				tempVar := numbers[i]
+				numbers[i] = numbers[j]
+				numbers[j] = tempVar
+			}
+		}
+	}
+	return numbers[2]
 }
