c664c81310a5e8a7e36e76445e7dd18bffed2710
Hamza Maach <hamzamaach56@gmail.com>
2024-02-29 11:54:58 +0100

    function that prints in descending order all possible combinations

--- descendcomb.go
+++ descendcomb.go
@@ -3,31 +3,31 @@ package piscine
 import (
 	"github.com/01-edu/z01"
 )
+
 func printRune(nb rune) {
-    z01.PrintRune(nb)
+	z01.PrintRune(nb)
 }
 
 func DescendComb() {
-    for a := '9'; a >= '0'; a-- {
-        for b := '9'; b >= '0'; b-- {
-            for c := '9'; c >= '0'; c-- {
-                for d := '9'; d >= '0'; d-- {
-                    if (a > c) || (b > d && a == c) {
-                        printRune(a)
-                        printRune(b)
-                        printRune(' ')
-                        printRune(c)
-                        printRune(d)
-                        if a == '0' && b == '1' && c == '0' && d == '0' {
-                            break
-                        } else {
-                            printRune(',')
-                            printRune(' ')
-                        }
-                    }
-                }
-            }
-        }
-    }
+	for a := '9'; a >= '0'; a-- {
+		for b := '9'; b >= '0'; b-- {
+			for c := '9'; c >= '0'; c-- {
+				for d := '9'; d >= '0'; d-- {
+					if (a > c) || (b > d && a == c) {
+						printRune(a)
+						printRune(b)
+						printRune(' ')
+						printRune(c)
+						printRune(d)
+						if a == '0' && b == '1' && c == '0' && d == '0' {
+							break
+						} else {
+							printRune(',')
+							printRune(' ')
+						}
+					}
+				}
+			}
+		}
+	}
 }
-
