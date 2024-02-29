dceee6b35b1573e80e7dfa891ba9e72015738742
Hamza Maach <hamzamaach56@gmail.com>
2024-02-29 14:13:41 +0100

    func that returns the number of steps necessary to reach 1

---
+++ collatzcountdown.go
@@ -0,0 +1,19 @@
+package piscine
+
+func CollatzCountdown(start int) int {
+	count := 0
+	nb := start
+	for nb > 0 {
+		if nb%2 == 0 {
+			nb /= 2
+			count++
+		} else {
+			nb = (nb * 3) + 1
+			count++
+		}
+		if nb == 1 {
+			break
+		}
+	}
+	return count
+}
