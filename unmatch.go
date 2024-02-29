1d9982b03c7d80ea6e6229e2d334c8176b651cfc
Hamza Maach <hamzamaach56@gmail.com>
2024-02-29 12:56:34 +0100

    function that returns the nb that does not have a correspondent pair in []int

--- unmatch.go
+++ unmatch.go
@@ -1,17 +1,17 @@
 package piscine
 
 func Unmatch(a []int) int {
-    n := -1
-    for _, nb := range a {
-        cnt := 0
-        for_, com := range a {
-            if nb == com {
-                cnt++
-            }
-        }
-        if cnt%2 != 0 {
-            return nb
-        }
-    }
-    return n
+	n := -1
+	for _, nb := range a {
+		cnt := 0
+		for _, com := range a {
+			if nb == com {
+				cnt++
+			}
+		}
+		if cnt%2 != 0 {
+			return nb
+		}
+	}
+	return n
 }
