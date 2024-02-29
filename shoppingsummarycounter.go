e1ca21e6c06914dc987af41bd70d5e6e7b82a3d5
Hamza Maach <hamzamaach56@gmail.com>
2024-02-29 17:22:17 +0100

    remove empty elements from slice of strings

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,16 +1,16 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-    elements := make(map[string]int)
-    var word string
-    for _, elem := range str {
-        if elem == ' ' {
-            elements[word] +=1
-            word = ""
-        } else if elem != ' ' {
-            word +=  string(elem)
-        }
-    }
-    elements[word]++
-    return elements
+	elements := make(map[string]int)
+	var word string
+	for _, elem := range str {
+		if elem == ' ' {
+			elements[word] += 1
+			word = ""
+		} else if elem != ' ' {
+			word += string(elem)
+		}
+	}
+	elements[word]++
+	return elements
 }
