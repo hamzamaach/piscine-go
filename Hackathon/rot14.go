d272df622d820106a125ec723d200ea5a2502b94
Hamza Maach <hamzamaach56@gmail.com>
2024-02-29 11:27:42 +0100

    function that prints in descending order all possible combinations

--- Hackathon/rot14.go
+++ Hackathon/rot14.go
@@ -1,5 +1,15 @@
 package piscine
 
 func Rot14(s string) string {
-	return s
+	var result string
+	for _, char := range s {
+		if char >= 'A' && char < 'M' || char >= 'a' && char < 'm' {
+			result += string(char + 14)
+		} else if char >= 'M' && char <= 'Z' || char >= 'm' && char <= 'z' {
+			result += string(char - 12)
+		} else {
+			result += string(char)
+		}
+	}
+	return result
 }
--- issorted.go
+++
@@ -1,7 +0,0 @@
-package piscine
-
-func IsSorted(f func(a, b int) int, a []int) bool {
-	var result bool
-	
-	return result
-}
