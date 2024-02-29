9d295ad2aac746149b96b9e32a51a45d158bf47c
Hamza Maach <hamzamaach56@gmail.com>
2024-02-29 18:22:00 +0100

    func that deals a pack of 12 cards evenly between 4 players

---
+++ dealapackofcards.go
@@ -0,0 +1,14 @@
+package piscine
+
+import "fmt"
+
+func DealAPackOfCards(deck []int) {
+	players := []string{"Player 1", "Player 2", "Player 3", "Player 4"}
+	cardsPerPlayer := 3
+	for i, player := range players {
+		startIndex := i * cardsPerPlayer
+		endIndex := (i + 1) * cardsPerPlayer
+		cardForPlayer := deck[startIndex:endIndex]
+		fmt.Printf("%s: %d, %d, %d\n", player,cardForPlayer[0],cardForPlayer[1],cardForPlayer[2])
+	}
+}
