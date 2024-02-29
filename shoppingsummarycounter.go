ed202de813549bb3fd2910bed0126455c2b4dd9a
Hamza Maach <hamzamaach56@gmail.com>
2024-02-29 16:36:57 +0100

    func that count the total amount of times each item in shopping

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,22 +1,21 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	// elements := []string{"Burger", "Water", "Carrot", "Coffee", "Water", "Water", "Chips", "Carrot", "Carrot", "Burger", "Carrot", "Water"}
 	elements := []string{}
-
 	word := ""
 	for _, char := range str {
 		if char != ' ' {
 			word += string(char)
 		} else {
-			elements = append(elements, word)
-			word = ""
+			if word != "" {
+				elements = append(elements, word)
+				word = ""
+			}
 		}
 	}
 	if word != "" {
 		elements = append(elements, word)
 	}
-
 	result := make(map[string]int)
 	for _, elem := range elements {
 		_, exists := result[elem]
