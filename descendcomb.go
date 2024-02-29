32501f0c468fdd675592b66d9dd334437febf3c1
Hamza Maach <hamzamaach56@gmail.com>
2024-02-29 11:34:14 +0100

    function that prints in descending order all possible combinations

--- descendcomb.go
+++ descendcomb.go
@@ -4,17 +4,16 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func PrintNumber(n int) {
-	if n < 10 {
-		z01.PrintRune('0')
-		z01.PrintRune(rune('0' + n))
-	} else {
-		z01.PrintRune(rune('0' + n/10))
-		z01.PrintRune(rune('0' + n%10))
-	}
-}
-
 func DescendComb() {
+	PrintNumber := func(n int) {
+		if n < 10 {
+			z01.PrintRune('0')
+			z01.PrintRune(rune('0' + n))
+		} else {
+			z01.PrintRune(rune('0' + n/10))
+			z01.PrintRune(rune('0' + n%10))
+		}
+	}
 	for i := 99; i >= 1; i-- {
 		for j := i - 1; j >= 0; j-- {
 			if i != 99 && j != 98 {
