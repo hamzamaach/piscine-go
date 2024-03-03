19ae6e24570b7008c89a51dffc6f520f5767771d
Hamza MAACH <hamzamaach56@gmail.com>
2024-03-03 11:13:39 +0100

    func that check if a slice of intigers is sorted

---
+++ issorted.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for i := 0; i < len(a)-1; i++ {
+		v := f(a[i], a[i+1])
+		if v <= 0 {
+			return false
+		}
+	}
+	return true
+}
