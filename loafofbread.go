fb2398eeab07436d03bc1908c50268434d6368e0
Hamza MAACH <hamzamaach56@gmail.com>
2024-03-05 11:18:14 +0100

    func that takes a str and returns another one with words of 5 chars and skips the next

--- loafofbread.go
+++ loafofbread.go
@@ -1,20 +1,21 @@
 package piscine
 
 func LoafOfBread(str string) string {
+	if len(str) == 0 {
+		return "\n"
+	}
 	if len(str) < 5 {
 		return "Invalid Output\n"
 	}
+	counter := 0
 	result := ""
-	wordLength := 0
 	for _, char := range str {
-		if char != ' ' {
-			if wordLength == 5 {
-				result += " "
-				wordLength = 1
-				continue
-			}
+		if char != ' ' && counter != 5 {
 			result += string(char)
-			wordLength++
+			counter++
+		} else if counter == 5 {
+			result += " "
+			counter = 0
 		}
 	}
 	return result + "\n"
