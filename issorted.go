03fd1348a583a2e6ce2e1c4c0e878d7dd87ffbd0
Hamza MAACH <hamzamaach56@gmail.com>
2024-03-05 10:50:54 +0100

    func that check if slice is sorted

--- issorted.go
+++ issorted.go
@@ -3,7 +3,13 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a)-1; i++ {
 		v := f(a[i], a[i+1])
-		if v <= 0 {
+		if v < 0 {
+			return false
+		}
+	}
+	for i := 0; i < len(a)-1; i++ {
+		v := f(a[i+1], a[i])
+		if v > 0 {
 			return false
 		}
 	}
