ce78be5f66bfa55c50a24be137f45305120e9c54
Hamza MAACH <hamzamaach56@gmail.com>
2024-03-05 10:54:46 +0100

    func that check if slice is sorted

--- issorted.go
+++ issorted.go
@@ -7,11 +7,5 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 			return false
 		}
 	}
-	for i := 0; i < len(a)-1; i++ {
-		v := f(a[i+1], a[i])
-		if v > 0 {
-			return false
-		}
-	}
 	return true
 }
