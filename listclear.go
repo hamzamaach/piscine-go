769732f1968ca0f5e691d79fe20dafab2d3d9b60
Hamza MAACH <hamzamaach56@gmail.com>
2024-03-06 14:35:18 +0100

    func that clear a linked list

--- listclear.go
+++ listclear.go
@@ -3,6 +3,9 @@ package piscine
 func ListClear(l *List) {
 	current := l.Head
 	for current != nil {
-		current = nil
+		current.Data = nil
+		current.Next = nil
+		current = current.Next
 	}
+	l.Head = nil
 }
