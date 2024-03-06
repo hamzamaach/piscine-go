ef1271117e9d43cf69eff4852f1e56e35eb8b80a
Hamza MAACH <hamzamaach56@gmail.com>
2024-03-06 14:14:18 +0100

    func that return the size of a linked list

---
+++ listsize.go
@@ -0,0 +1,21 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListSize(l *List) int {
+    size := 0
+    current := l.Head
+    for current != nil {
+        size++
+        current = current.Next
+    }
+    return size
+}
