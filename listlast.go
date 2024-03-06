4e758b1a249796bd2ce3bffee4e04cf6ad253d3e
Hamza MAACH <hamzamaach56@gmail.com>
2024-03-06 14:23:26 +0100

    get the last data of a linked list

---
+++ listlast.go
@@ -0,0 +1,24 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListLast(l *List) interface{} {
+	var result interface{}
+	current := l.Head
+	for current != nil {
+		if current.Next == nil {
+			result = current.Data
+			break
+		}
+		current = current.Next
+	}
+	return result
+}
