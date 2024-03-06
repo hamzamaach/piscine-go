2afbcc223c5e160584b2872e2587ab4b6344d44e
Hamza MAACH <hamzamaach56@gmail.com>
2024-03-06 14:45:02 +0100

    func that return func in given position

---
+++ listat.go
@@ -0,0 +1,20 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+func ListAt(l *NodeL, pos int) *NodeL {
+	current := l
+	count := 0
+	for current != nil {
+		if pos == count {
+			return current
+		}
+		current = current.Next
+		count++
+	}
+	return nil
+
+}
